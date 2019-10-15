b5f2b5983606d370e39b9e86549d5d64dd8d00b7
Dumar Ojeda <dumarojeda@gmail.com>
2019-10-14 22:08:32 -0500

    Manejando enlaces

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
 
 const Layout = ({children}) => (
   <div className="App">
@@ -8,6 +8,6 @@ const Layout = ({children}) => (
     {children}
     <Footer />
   </div>
-);
+)
 
-export default Layout;
+export default Layout
