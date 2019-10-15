13ae27456f53002e24cc39c0561e74d243d9de4c
Dumar Ojeda <dumarojeda@gmail.com>
2019-10-14 22:00:24 -0500

    Agregando Layout

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -13,37 +13,36 @@ const API = 'http://localhost:3000/initalState'
 const Home = () => {
   const initalState = useInitialState(API)
   return initalState.length === 0 ? <h1>Loading...</h1> : (
-    <div className="App">
-      <Header />
-      <Search />
-      {initalState.mylist.length > 0 &&
-        <Categories title="Mi Lista">
+    <>
+      <div className="App">
+        <Search />
+        {initalState.mylist.length > 0 &&
+          <Categories title="Mi Lista">
+            <Carousel>
+              {initalState.mylist.map(item =>
+                <CarouselItem key={item.id} {...item} />
+              )}
+            </Carousel>
+          </Categories>
+        }
+
+        <Categories title="Tendencias">
           <Carousel>
-            {initalState.mylist.map(item =>
+            {initalState.trends.map(item =>
               <CarouselItem key={item.id} {...item} />
             )}
           </Carousel>
         </Categories>
-      }
 
-      <Categories title="Tendencias">
-        <Carousel>
-          {initalState.trends.map(item =>
-            <CarouselItem key={item.id} {...item} />
-          )}
-        </Carousel>
-      </Categories>
-
-      <Categories title="Originales de Platzi Video">
-        <Carousel>
-          {initalState.originals.map(item =>
-            <CarouselItem key={item.id} {...item} />
-          )}
-        </Carousel>
-      </Categories>
-
-      <Footer />
-    </div>
+        <Categories title="Originales de Platzi Video">
+          <Carousel>
+            {initalState.originals.map(item =>
+              <CarouselItem key={item.id} {...item} />
+            )}
+          </Carousel>
+        </Categories>
+      </div>
+    </>
   )
 }
 
