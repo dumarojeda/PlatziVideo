fbc54e21626d840839619912b71adedf640bb802
Dumar Ojeda​ <dumar.ojeda@geometry.com>
2019-10-21 18:03:20 -0500

    Agregando delete

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,15 @@
 const reducer = (state, action) => {
   switch (action.type) {
     case 'SET_FAVORITE':
-    return {
-      ...state,
-      myList: [...state.myList, action.payload]
-    }
+      return {
+        ...state,
+        myList: [...state.myList, action.payload]
+      }
+    case 'DELETE_FAVORITE':
+      return {
+        ...state,
+        myList: state.myList.filter(items => items.id !== action.payload)
+      }
     default:
       return state
   }
